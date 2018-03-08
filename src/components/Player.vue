5a0e43a36ac12f45a26f1aa9f813c84f80b94d33
daiki <dik.piyo.piyo@gmail.com>
2018-03-08 18:50:43 +0900

    use vue-cli

---
+++ src/components/Player.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="player">
+    <div>
+      <youtube id="youtube" :video-id="videoId" :height="height" :width="width" :player-vars="playerVars" ref="youtube" @ready="ready"></youtube>
+      <div id="cover"></div>
+    </div>
+    <div id="control">
+      <button @click="playVideo">play</button>
+      <button @click="pauseVideo">pause</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+
+Vue.use(VueYoutube)
+
+const VIDEO_ID = 'tpxVMAu1O0Q'
+
+export default {
+  name: 'player',
+  data () {
+    return {
+      height: window.innerHeight,
+      width: window.innerWidth,
+      videoId: VIDEO_ID,
+      playerVars: {
+        controls: 0,
+        modestbranding: 1,
+        rel: 0,
+        showinfo: 0
+      }
+    }
+  },
+  created: function () {
+    window.addEventListener('resize', this.resize, false)
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.resize, false)
+  },
+  methods: {
+    playVideo () {
+      this.player.playVideo()
+    },
+    pauseVideo () {
+      this.player.pauseVideo()
+    },
+    resize () {
+      this.player.setSize(window.innerWidth, window.innerHeight)
+    },
+    ready () {
+      this.player.mute()
+      this.player.playVideo()
+    }
+  },
+  computed: {
+    player () {
+      return this.$refs.youtube.player
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+#youtube, #cover, #control {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+}
+
+#youtube {
+  z-index: -1;
+}
+
+#cover {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0);
+  z-index: 0;
+}
+
+#control {
+  z-index: 1;
+}
+</style>
