b8ec1ecabb73cb2b305d2514ed583ebb06831ab2
daiki <dik.piyo.piyo@gmail.com>
2018-03-12 18:20:43 +0900

    test frameless

--- src/main/index.js
+++ src/main/index.js
@@ -20,6 +20,7 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
+    // frame: false,
     height: 563,
     useContentSize: true,
     width: 1000
